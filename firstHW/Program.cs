b1a2cf8ce50946d4d94c171edb255f53418b779d
NoroMedafe <danya1515@inbox.ru>
2022-04-06 11:24:53 +0300

    Добавьте файлы проекта.

---
+++ firstHW/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string enemyName;
+            int userLife;
+            int enemyLife;
+            short userDamage;
+            short userArmor;
+            short enemyDamage;
+            short enemyArmor;
+            int countItemUser;
+            bool bagfull;
+        }
+    }
+}
